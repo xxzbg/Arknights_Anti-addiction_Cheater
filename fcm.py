d4b74b1b3dd296109fe6e5c3d5843ddf090377f8
Tao0Lu <35399423+Tao0Lu@users.noreply.github.com>
2020-08-18 18:17:00 +0800

    Update fcm.py

--- fcm.py
+++ fcm.py
@@ -16,25 +16,24 @@ class fcm:
     def response(self, flow: mitmproxy.http.HTTPFlow):
         global entryGame
         if flow.request.url.startswith("https://ak-fs.hypergryph.com/announce/Android/preannouncement.meta.json") or flow.request.url.startswith("https://ak-fs.hypergryph.com/announce/IOS/preannouncement.meta.json"):
-            entryGame=True
+            entryGame=True #进入游戏开始唤醒界面都会调用这个
         if flow.request.url.startswith("https://ak-as.hypergryph.com:9443/online/v1/ping"):
             j=json.loads(flow.response.get_text())
-            print('防沉迷破解 游戏时间:修改为--> 不限制')
+            print('明日方舟防沉迷破解: 禁用 防沉迷-游戏剩余时间')
             if entryGame:
                 flow.response.set_text('{"result": 0, "message": "OK", "interval": 60, "timeLeft": 5400, "alertTime": 600}')
                 entryGame=False
             else:
                 flow.response = http.HTTPResponse.make(404)
             if j['result']==3:
-                print('防沉迷破解 已达到本日在线时长上限或不在可游戏时间范围内，破解仍可以继续游戏，但请合理安排游戏时间')
+                print('明日方舟防沉迷破解: 您已达到本日在线时长上限或不在可游戏时间范围内，破解后仍可以继续游戏，但请合理安排游戏时间。')
             else:
                 s = 5400-j['timeLeft']
                 h = int(s/3600)
                 m = int((s-h*3600)/60)
                 ss = int(s-h*3600-m*60)
-                print ('防沉迷破解 您已在线'+str(h)+'小时'+str(m)+'分钟' + str(ss)+'秒，请合理安排游戏时间')
-                
-            
+                print ('明日方舟防沉迷破解: 您已在线'+str(h)+'小时'+str(m)+'分钟' + str(ss)+'秒，请合理安排游戏时间。')
+
 addons = [
     fcm()
 ]
