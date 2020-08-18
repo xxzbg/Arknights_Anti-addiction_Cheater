0fa1dc634d8af3582446f1347b7cfda926ec2273
Tao0Lu <35399423+Tao0Lu@users.noreply.github.com>
2020-08-18 18:03:16 +0800

    Update fcm.py

--- fcm.py
+++ fcm.py
@@ -22,18 +22,18 @@ class fcm:
             print('防沉迷破解 游戏时间:修改为--> 不限制')
             if entryGame:
                 flow.response.set_text('{"result": 0, "message": "OK", "interval": 60, "timeLeft": 5400, "alertTime": 600}')
-                print('防沉迷破解 已达到本日在线时长上限或不在可游戏时间范围内，破解仍可以继续游戏，但请合理安排游戏时间')
                 entryGame=False
             else:
-                if j['result']==3:
-                    print('防沉迷破解 已达到本日在线时长上限或不在可游戏时间范围内，破解仍可以继续游戏，但请合理安排游戏时间')
-                else:
-                    s = 5400-j['timeLeft']
-                    h = int(s/3600)
-                    m = int((s-h*3600)/60)
-                    ss = int(s-h*3600-m*60)
-                    print ('防沉迷破解 您已在线:'+str(h)+'小时'+str(m)+'分钟' + str(ss)+'秒，请合理安排游戏时间')
                 flow.response = http.HTTPResponse.make(404)
+            if j['result']==3:
+                print('防沉迷破解 已达到本日在线时长上限或不在可游戏时间范围内，破解仍可以继续游戏，但请合理安排游戏时间')
+            else:
+                s = 5400-j['timeLeft']
+                h = int(s/3600)
+                m = int((s-h*3600)/60)
+                ss = int(s-h*3600-m*60)
+                print ('防沉迷破解 您已在线'+str(h)+'小时'+str(m)+'分钟' + str(ss)+'秒，请合理安排游戏时间')
+                
             
 addons = [
     fcm()
